1952fe9d2ada721825413db5a56288544355afc5
sabin.nakarmi223 <asbn2231@gmail.com>
2024-06-14 17:09:31 +0545

    NEW NEW

---
+++ firebase-messaging-sw.js
@@ -0,0 +1,38 @@
+importScripts("https://www.gstatic.com/firebasejs/10.12.2/firebase-app-compat.js");
+importScripts("https://www.gstatic.com/firebasejs/10.12.2/firebase-messaging-compat.js");
+
+//import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: 'AIzaSyBmfKTuQu5CbHMrz9xqTaLN1fRTR77mJZc',
+    appId: '1:410288988185:web:8a6a65e3e56e32ff0e0a88',
+    messagingSenderId: '410288988185',
+    projectId: 'creators-mela',
+    authDomain: 'creators-mela.firebaseapp.com',
+    storageBucket: 'creators-mela.appspot.com',
+    measurementId: 'G-92MF6L01HB',
+};
+
+// Initialize Firebase
+const app = firebase.initializeApp(firebaseConfig);
+//const analytics = getAnalytics(app);
+
+const messaging = firebase.messaging();
+
+  /*messaging.onMessage((payload) => {
+  console.log('Message received. ', payload);*/
+  messaging.onBackgroundMessage(function(payload) {
+    console.log('Received background message ', payload);
+
+    const notificationTitle = payload.notification.title;
+    const notificationOptions = {
+      body: payload.notification.body,
+    };
+
+    self.registration.showNotification(notificationTitle,
+      notificationOptions);
+  });
