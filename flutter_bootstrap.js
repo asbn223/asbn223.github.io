6d1e197bb86ca48080ea43b78509c5d71b7ec875
asbn223 <asbn2231@gmail.com>
2025-04-17 17:09:49 +0545

    Main PWA

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"18b71d647a292a980abb405ac7d16fe1f0b204
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "868715501"
+    serviceWorkerVersion: "110175607"
   }
 });
